ac6f64a46bae4ab09294b7717b149c67699a38c7
vixen-vdenae21 <vixen-vdenae@rambler.ru>
2021-06-18 16:15:40 +0300

    change js && style.css

--- script/script.js
+++ script/script.js
@@ -25,7 +25,8 @@ let arrow = document.querySelector('.arrow')
 let hideBlock = function () {
   buttonShow.style.display = 'block';
   buttonHidden.style.display = 'none';
-  swiperContainer.style.height = 290 + 'px'
+  swiperContainer.style.height = 270 + 'px'
+  // swiperContainer.style.height = '100%'
   for(let i = 0; i < swiperItemCollection.length; i++){
     swiperItemCollection[i].style.display = 'none';
   }
@@ -36,9 +37,23 @@ function showHide() {
   for(let i = 0; i < swiperItemCollection.length; i++){
     swiperItemCollection[i].style.display = 'flex';
   }
-  swiperContainer.style.height = 375 + 'px'
+  swiperContainer.style.height = 410 + 'px'
+  // swiperContainer.style.height = '100%'
   buttonShow.style.display = 'none';
   buttonHidden.style.display = 'block';
   swiperContainer.style.background = '#F2F2F2'
   arrow.classList.add('transformArrowImage')
 }
+
+document.addEventListener("DOMContentLoaded", function(event)
+{
+  window.onresize = function() {
+    let widthWind = document.querySelector('body').offsetWidth;
+    if ((widthWind <= 750)){
+      swiperContainer.style.height = 200 + 'px'
+
+    }else {
+      swiperContainer.style.height =  410 + 'px'
+    }
+  };
+});
